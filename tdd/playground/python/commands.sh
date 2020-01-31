f8a74efddd7901bdf740172e63b02db8765e0a20
Eranga Jayalatharachchi <dumith.eranga@gmail.com>
2020-01-31 11:21:05 +0530

    added tdd playground structure for python

---
+++ tdd/playground/python/commands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+exit;
+
+# create the container to run the project
+sudo docker run -it --detach --name tddpython --mount type=bind,source="$(pwd)"/,target=/app python:latest
+
+# log in to the container 
+sudo docker exec -it tddpython /bin/bash
+
+## inside the container
+
+    # navigate to the project directory
+    cd /app
+
+    pip install -r requirements.txt
+    # or 
+    pip install pytest
+
+    # run the unit tests
+    pytest
+    # or
+    # keep the unit tests running
+    chmod +x run-tests-forever.sh
+    ./run-tests-forever.sh
+
+
+# remove the container
+sudo docker kill tddpython && sudo docker rm tddpython
+
---
+++ tdd/playground/python/run-tests-forever.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while true;
+do clear; pytest; sleep 2;
+done
