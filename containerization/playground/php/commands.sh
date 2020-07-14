5929d71c262f70607f5ff8d841d83ef4df3667af
Eranga Jayalatharachchi <dumith.eranga@gmail.com>
2020-07-14 13:49:07 +0530

    added containerization playground php

---
+++ containerization/playground/php/commands.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo docker build -t app:base docker/base/
+sudo docker build -t app:latest docker/app/
+sudo docker run -it -v `pwd`/src:/app -p 8081:8081 --rm --name myapp app:latest
+
+
+# curl http://0.0.0.0:8081
