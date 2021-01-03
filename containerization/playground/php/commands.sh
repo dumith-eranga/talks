e5ba3877578f9261a830a88168fa422e9b24b98c
Eranga Jayalatharachchi <dumith.eranga@gmail.com>
2021-01-03 14:03:58 +0530

    WIP some technical for done some time ago

--- containerization/playground/php/commands.sh
+++ containerization/playground/php/commands.sh
@@ -4,5 +4,6 @@ sudo docker build -t app:base docker/base/
 sudo docker build -t app:latest docker/app/
 sudo docker run -it -v `pwd`/src:/app -p 8081:8081 --rm --name myapp app:latest
 
+# sudo docker exec -it myapp /bin/bash
 
 # curl http://0.0.0.0:8081
